2125276b24487487fb3ccd5a6c220d23f3670fd1
choiyungsik <choiyungsik@github.com>
2020-01-29 04:22:30 +0900

    edit src file

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -126,6 +126,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/choiys/stauto_ws/build/gtest/cmake_install.cmake")
+  include("/home/choiys/stauto_ws/build/robot_description/cmake_install.cmake")
 
 endif()
 
