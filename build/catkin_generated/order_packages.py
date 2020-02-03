d73a53318830e7e23d9c28139edddbf640eb7bcd
choiyungsik <choiyungsik@github.com>
2020-02-03 19:34:58 +0900

    stauto_control

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/choiys/stauto_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/choiys/stauto_ws/devel;/opt/ros/kinetic".split(';') if "/home/choiys/stauto_ws/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/choiys/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/choiys/catkin_ws/devel;/opt/ros/kinetic" != "" else []
---
+++ build/stauto_control/CMakeFiles/main_control_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_control_generate_messages_eus"
+  "/home/choiys/stauto_ws/devel/share/roseus/ros/main_control/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/main_control_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
