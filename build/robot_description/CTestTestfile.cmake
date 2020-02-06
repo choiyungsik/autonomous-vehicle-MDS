414c46654e0e776eb89a1c61c2e1719459a6eec6
anseongje <agc770@naver.com>
2020-02-06 20:26:08 +0900

    stauto

--- build/robot_description/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anseongje/stauto_ws/src/robot_description
-# Build directory: /home/anseongje/stauto_ws/build/robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(_ctest_robot_description_roslaunch-check_launch "/home/anseongje/stauto_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python2" "/opt/ros/kinetic/share/catkin/cmake/test/run_tests.py" "/home/anseongje/stauto_ws/build/test_results/robot_description/roslaunch-check_launch.xml" "--return-code" "/usr/bin/cmake -E make_directory /home/anseongje/stauto_ws/build/test_results/robot_description" "/opt/ros/kinetic/share/roslaunch/cmake/../scripts/roslaunch-check -o '/home/anseongje/stauto_ws/build/test_results/robot_description/roslaunch-check_launch.xml' '/home/anseongje/stauto_ws/src/robot_description/launch' ")
--- build/stauto_sensor/catkin_generated/stauto_sensor-msg-extras.cmake.installspace.in
+++
@@ -1,2 +0,0 @@
-set(stauto_sensor_MESSAGE_FILES "")
-set(stauto_sensor_SERVICE_FILES "")
