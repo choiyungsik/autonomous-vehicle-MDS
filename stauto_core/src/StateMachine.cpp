ba00bacf4881a5c04068182f6268b03b19e68fa3
choiyungsik <cube0103@naver.com>
2020-06-10 13:24:03 +0900

    navigation_state_machine

---
+++ stauto_core/src/StateMachine.cpp
@@ -0,0 +1,11 @@
+#include <ros/ros.h>
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <StateMachine.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc,argv,"state_machine_node");
+    return 0;
+}
--- stauto_core/src/core_test_keyboard.py
+++ stauto_core/src/core_test_keyboard.py
@@ -6,16 +6,24 @@
 
 import sys, select, termios, tty, math
 import rospy
-from std_msgs.msg import UInt8
+from std_msgs.msg import Bool
+from std_msgs.msg import ByteMultiArray, Int32MultiArray
+from std_msgs.msg import MultiArrayDimension
 from enum import Enum
 
 header_msgs = """
 Make a interruption by typing!!
 --------------------------------
+c : cruise
+o : obstacle detected
 p : parking_sign detected
 t : traffic_light detected
-r : red_light detected
-g : green_light detected
+f : safety_zone detected
+r : red_light 
+g : green_light
+a : straightleft
+l : left
+s : stop
 
 CTRL_C to quit
 """
@@ -30,16 +38,71 @@ def GetKey():
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
+def fnPublish(key):
+    global pub_avoidance,pub_stop,pub_traffic,pub_parking,pub_safety,TrafficArray
+
+    parking = Bool()
+    stop = Bool()
+    obstacle = Bool()
+    safety = Bool()
+
+    if key == 'p':
+        parking.data = True
+        pub_parking.publish(parking)
+    elif key == 'o':
+        obstacle.data = True
+        pub_avoidance.publish(obstacle)
+    elif key == 'f':
+        safety.data = True
+        pub_safety.publish(safety)
+    elif key == 'r':
+        TrafficArray.data[0] = 1
+        pub_traffic.publish(TrafficArray)
+    elif key == 'g':
+        TrafficArray.data[1] = 1
+        pub_traffic.publish(TrafficArray)
+    elif key == 'l':
+        TrafficArray.data[2] = 1
+        pub_traffic.publish(TrafficArray)
+    elif key == 'a':
+        TrafficArray.data[3] = 1
+        pub_traffic.publish(TrafficArray)
+    elif key == 's':
+        stop.data = True
+        pub_stop.publish(stop)
+
+    #pub_traffic.publish(TrafficArray)
+    for j in range(4):
+        TrafficArray.data[j]= 0
+    
+    parking.data = False
+    stop.data = False
+    obstacle.data = False
+    safety.data = False
+
 
 if __name__ == "__main__":
+    global Machine_State, TrafficArray,pub_avoidance,pub_stop,pub_traffic,pub_parking,pub_safety
     settings = termios.tcgetattr(sys.stdin)
 
     rospy.init_node('stauto_core_test_keyboard')
 
-    Machine_State = Enum('Machine_state', 'parking traffic traffic_red traffic_green cruise')
+    Machine_State = Enum('Machine_state', 'cruise avoid_cruise stop traffic parking safety_zone')
     state_event = Machine_State.cruise.value
-    
-    pub_transition = rospy.Publisher('state_transition',UInt8,queue_size=1)
+    array = [0,0,0,0]
+    TrafficArray = Int32MultiArray()
+    TrafficArray.layout.dim.append(MultiArrayDimension())
+    TrafficArray.layout.dim[0].label = "traffic_mode"
+    TrafficArray.layout.dim[0].size = 4
+    TrafficArray.layout.dim[0].stride = 4
+    TrafficArray.layout.data_offset = 0
+    TrafficArray.data=[0]*4
+
+    pub_avoidance = rospy.Publisher('/detect/obstacle',Bool,queue_size=1)
+    pub_stop = rospy.Publisher('/detect/stop_sign',Bool,queue_size=1)
+    pub_parking = rospy.Publisher('/detect/parking_sign',Bool,queue_size=1)
+    pub_safety = rospy.Publisher('/detect/safety_sign',Bool,queue_size=1)
+    pub_traffic = rospy.Publisher('/detect/traffic_sign',Int32MultiArray,queue_size=1)
 
     loop_rate = rospy.Rate(10) # 10hz
 
@@ -47,32 +110,12 @@ if __name__ == "__main__":
         print(header_msgs)
         while(1):
             key = GetKey()
-            if key == 'p':
-                print('Parking sign is detected')
-                state_event = Machine_State.parking.value
+            fnPublish(key)
             
-            elif key == 't':
-                print('Traffic light is detected')
-                state_event = Machine_State.traffic.value
-
-            elif key == 'r':
-                print('The traffic light is red!!')
-                state_event = Machine_State.traffic_red.value
-
-            elif key == 'g':
-                print('The traffic light is turned into green')
-                state_event = Machine_State.traffic_green.value
-            
-            elif key == '\x03': #CTRL + C
+            if key == '\x03': #CTRL + C
                 break
-            
-            pub_transition.publish(state_event)
 
     except Exception as e:
         print(e)
     
-    finally:
-        state_event = Machine_State.cruise.value
-        pub_transition.publish(state_event)
-
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
