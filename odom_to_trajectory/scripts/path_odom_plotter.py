f4733136fe7d4ec34b9867c0c954c5d15a9b332f
choiyungsik <cube0103@naver.com>
2020-06-10 13:24:03 +0900

    navigation_state_machine

--- odom_to_trajectory/scripts/path_odom_plotter.py
+++ odom_to_trajectory/scripts/path_odom_plotter.py
@@ -4,7 +4,7 @@ import rospy
 from tf.transformations import quaternion_from_euler
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry, Path
-from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped, Pose
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
 from sensor_msgs.msg import Joy
 
 import sys
@@ -25,7 +25,7 @@ def callback(data):
         pose = PoseStamped()    
 
     #Set a atributes of the msg
-        pose.header.frame_id = "base_link"
+        pose.header.frame_id = "odom"
         pose.pose.position.x = float(data.pose.pose.position.x)
         pose.pose.position.y = float(data.pose.pose.position.y)
         pose.pose.orientation.x = float(data.pose.pose.orientation.x)
@@ -37,10 +37,7 @@ def callback(data):
         if (xAnt != pose.pose.position.x and yAnt != pose.pose.position.y):
                 #Set a atributes of the msg
                 pose.header.seq = path.header.seq + 1
-                if(amcl_mode):
-                    path.header.frame_id="map"
-                else:
-                    path.header.frame_id="odom"
+                path.header.frame_id="odom"
                 path.header.stamp=rospy.Time.now()
                 pose.header.stamp = path.header.stamp
                 path.poses.append(pose)
@@ -48,7 +45,7 @@ def callback(data):
 
         cont=cont+1
 
-        # rospy.loginfo("Valor del contador: %i" % cont)
+        rospy.loginfo("Valor del contador: %i" % cont)
         if cont>max_append:
         	path.poses.pop(0)
 
@@ -59,49 +56,7 @@ def callback(data):
         yAnt=pose.pose.position.y
         return path
 
-def callback_enc(data_enc):
-        global xAnt_enc
-        global yAnt_enc
-        global cont_enc
 
-    #Is created the pose msg, its necessary do it each time because Python manages objects by reference, 
-        #and does not make deep copies unless explicitly asked to do so.
-        pose_enc = PoseStamped()    
-
-    #Set a atributes of the msg
-        pose_enc.header.frame_id = "base_footprint"
-        pose_enc.pose.position.x = float(data_enc.pose.pose.position.x)
-        pose_enc.pose.position.y = float(data_enc.pose.pose.position.y)
-        pose_enc.pose.orientation.x = float(data_enc.pose.pose.orientation.x)
-        pose_enc.pose.orientation.y = float(data_enc.pose.pose.orientation.y)
-        pose_enc.pose.orientation.z = float(data_enc.pose.pose.orientation.z)
-        pose_enc.pose.orientation.w = float(data_enc.pose.pose.orientation.w)
-
-    #To avoid repeating the values, it is found that the received values are differents
-        if (xAnt_enc != pose_enc.pose.position.x and yAnt_enc != pose_enc.pose.position.y):
-                #Set a atributes of the msg
-                pose_enc.header.seq = path_enc.header.seq + 1
-                if(amcl_mode):
-                    path_enc.header.frame_id="start"
-                else:
-                    path_enc.header.frame_id="odom"
-                path_enc.header.stamp=rospy.Time.now()
-                pose_enc.header.stamp = path_enc.header.stamp
-                path_enc.poses.append(pose_enc)
-                #Published the msg
-
-        cont_enc=cont_enc+1
-
-        # rospy.loginfo("Valor del contador: %i" % cont_enc)
-        if cont_enc>max_append:
-        	path_enc.poses.pop(0)
-
-        pub_enc.publish(path_enc)
-
-    #Save the last position
-        xAnt_enc=pose_enc.pose.orientation.x
-        yAnt_enc=pose_enc.pose.position.y
-        return path_enc
 
 
 if __name__ == '__main__':
@@ -109,46 +64,33 @@ if __name__ == '__main__':
         global xAnt
         global yAnt
         global cont
-        global xAnt_enc
-        global yAnt_enc
-        global cont_enc
         xAnt=0.0
         yAnt=0.0
         cont=0
-        xAnt_enc=0.0
-        yAnt_enc=0.0
-        cont_enc=0
+
 
 
         #Node and msg initialization
         rospy.init_node('path_odom_plotter')
 
-        amcl_mode = bool(rospy.get_param('~use_amcl', False)) # 
 
         #Rosparams that are set in the launch
         #max size of array pose msg from the path
-        # if not rospy.has_param("~max_list_append"):
-        #         rospy.logwarn('The parameter max_list_append dont exists')
-        # max_append = rospy.set_param("~max_list_append",10000000) 
-        max_append = 10000000
+        if not rospy.has_param("~max_list_append"):
+                rospy.logwarn('The parameter max_list_append dont exists')
+        max_append = rospy.set_param("~max_list_append",1000) 
+        max_append = 100000
         if not (max_append > 0):
                 rospy.logwarn('The parameter max_list_append not is correct')
                 sys.exit()
         pub = rospy.Publisher('/odompath', Path, queue_size=1)
-        pub_enc = rospy.Publisher('/odompath/enc', Path, queue_size=1)
 
 
         path = Path() #creamos el mensaje path de tipo path 
-        path_enc = Path()
         msg = Odometry()
-        msg_enc = Odometry()
 
         #Subscription to the topic
-        if(amcl_mode):
-            msg = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, callback) 
-        else:
-            msg = rospy.Subscriber('/odom', Odometry, callback) 
-        msg_enc = rospy.Subscriber('/odom/enc', Odometry, callback_enc) 
+        msg = rospy.Subscriber('/odom', Odometry, callback) 
 
         rate = rospy.Rate(30) # 30hz
 
--- stauto_control/src/teleop_keyboard.py
+++ stauto_control/src/teleop_keyboard.py
@@ -46,26 +46,16 @@ def getKey(safety_mode):
 def showInfo(speed_bound, angle_bound):
     return "current bounds:\tspeed %s\tangle %s " % (speed_bound, angle_bound)
 
-def cal_odometry(xpos,ypos,yaw,xvel,yvel):
-    odom_msg = Odometry()
+def cal_odometry(xpos,ypos,odom_quat,xvel,yvel):
+    global odom
 
-    odom_msg.header.stamp = rospy.Time.now()
-    odom_msg.header.frame_id = "odom"
-    odom_msg.child_frame_id = "base_link"
+    odom.header.stamp = rospy.Time.now()
+    odom.header.frame_id = "odom"
+    odom.child_frame_id = "base_footprint"
 
+    odom.pose.pose = Pose(Point(xpos,ypos,0.0),Quaternion(*odom_quat))
 
-    odom_msg.pose.pose.position.x = xpos
-    odom_msg.pose.pose.position.y = ypos
-    odom_msg.pose.pose.position.z = 0.0
-
-    odom_msg.pose.pose.orientation = tf.transformations.quaternion_from_euler(0,0,yaw)
-
-    odom_msg.twist.twist.linear.x = xvel
-    odom_msg.twist.twist.linear.y = yvel
-    odom_msg.twist.twist.angular.z = 0.0
-
-    return odom_msg
-
+    odom.twist.twist = Twist(Vector3(xvel,yvel,0),Vector3(0.0,0.0,0.0))
 
 class initpose:
     def __init__(self):
@@ -82,9 +72,9 @@ class initpose:
 
         print("init")
 
-
 # Main Func
 if __name__=="__main__":
+    global odom
     settings = termios.tcgetattr(sys.stdin)
     
     rospy.init_node('stauto_teleop')
@@ -92,25 +82,25 @@ if __name__=="__main__":
     
     #pub_cmd = rospy.Publisher('/ackermann_cmd', AckermannDriveStamped, queue_size=5)
     #pub_safe = rospy.Publisher('/ackermann_safe', AckermannDriveStamped, queue_size=5)
-    pub_odom = rospy.Publisher('/odom_tele',Odometry)
+    pub_odom = rospy.Publisher('/odom',Odometry,queue_size=1)
 
     safe_mode = bool(rospy.get_param('~safety_mode', False)) # true for safety cmds 
-    speed_i = float(rospy.get_param('~speed_incremental', 0.1)) # m/s
-    angle_i = float(rospy.get_param('~angle_incremental', 5.0*math.pi/180.0)) # rad (=5 degree)
-    speed_bound = float(rospy.get_param('~speed_bound', 2.0))
-    angle_bound = float(rospy.get_param('~angle_bound', 20.0*math.pi/180.0))
+    speed_i = float(rospy.get_param('~speed_incremental', 0.5)) # m/s
+    angle_i = float(rospy.get_param('~angle_incremental', 0.2*math.pi/180.0)) # rad (=1 degree)
+    speed_bound = float(rospy.get_param('~speed_bound', 3.0))
+    angle_bound = float(rospy.get_param('~angle_bound', 29.0*math.pi/180.0))
 
     x = 0.0
     y = 0.0
     th = 0.0
     tau = 0.0
+    beta = 0.0
     current_time = rospy.Time.now()
     last_time = rospy.Time.now()
 
     class_init_pose = initpose()
 
     #setting map and odom tf
-    
     
     if safe_mode:
         print ("Switched to Safety Mode !")
@@ -133,17 +123,15 @@ if __name__=="__main__":
     acc = 0.1
     target_speed = 0.0 # m/s
     target_angle = 0.0 # rad
-    # Create AckermannDriveStamped msg object
-    ackermann_msg = AckermannDriveStamped()
     odom = Odometry()
 
     try:
         print(header_msg)
         print(showInfo(speed_bound, angle_bound))
-        while(1):
+        while not rospy.is_shutdown():
             current_time = rospy.Time.now()
 
-            dt= (current_time - last_time).to_sec()
+            dt= (current_time - last_time).to_sec() + (current_time - last_time).to_nsec()*1e-9
 
             key = getKey(safe_mode)
             if key in moveBindings.keys():
@@ -162,9 +150,9 @@ if __name__=="__main__":
             elif key == 's' : # switch safety mode
                 safe_mode = not safe_mode
                 if safe_mode:
-                    print ("Switched to Safety Mode !")
+                    print ("Switched to Safety Mode!")
                 else:
-                    print ("Back to Standard Mode !")
+                    print ("Back to Standard Mode!")
             elif key == '\x03': # cltr + C
                 break
 
@@ -178,36 +166,38 @@ if __name__=="__main__":
             if target_angle < -angle_bound:
                 target_angle = -angle_bound
 
-            beta = atan((0.7/1.4)*tan(target_angle))
+            print("current speed : ", target_speed)
+            print("current angle : ",(target_angle * 180.0) /math.pi)
 
-            delta_tau = (target_speed/0.7)*sin(beta)
+            beta = atan((0.525/1.05)*tan(target_angle))
+
+            delta_tau = (target_speed/0.525)*sin(beta)
             tau += delta_tau
 
             delta_x = (target_speed * cos(tau + beta)) * dt
             delta_y = (target_speed * sin(tau + beta)) * dt
 
-            # Publishing command
-            #ackermann_msg.header.stamp = rospy.Time.now() # for future multi-cars applicaton
             x += delta_x
             y += delta_y
 
             odom_quat = tf.transformations.quaternion_from_euler(0, 0, tau)
-
+            
+            odom_broadcaster.sendTransform(
+                (x, y, 0.),
+                odom_quat,
+                current_time,
+                "base_footprint",
+                "odom"
+            )
             odom_broadcaster.sendTransform(
                 class_init_pose.position,
                 class_init_pose.orientation_quaternion,
                 current_time,
-                "odom",
+                "odom",          
                 "map"
             )
-            odom_broadcaster.sendTransform(
-                (x, y, 0.2),
-                odom_quat,
-                current_time,
-                "base_link",
-                "odom"
-            )
-            odom = cal_odometry(x,y,tau,target_speed * cos(tau + beta),target_speed * sin(tau + beta))
+
+            cal_odometry(x,y,odom_quat,target_speed * cos(tau + beta),target_speed * sin(tau + beta))
             pub_odom.publish(odom)
 
             last_time = current_time
@@ -216,7 +206,6 @@ if __name__=="__main__":
         print(e)
 
     finally:
-        ackermann_msg.drive.speed = 0
-        ackermann_msg.drive.steering_angle = 0
+        x = 0
 
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
