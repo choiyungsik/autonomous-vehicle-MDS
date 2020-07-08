6079f06b24e1d5d29d789d3a6d573f9728901022
choiyungsik <cube0103@naver.com>
2020-07-08 13:58:53 +0900

    add stauto core

--- gps_global_planner/src/RAstar_ros.cpp
+++ gps_global_planner/src/RAstar_ros.cpp
@@ -113,7 +113,7 @@ bool RAstarPlannerROS::makePlan(const geometry_msgs::PoseStamped& start, const g
                              std::vector<geometry_msgs::PoseStamped>& plan)
 {
     string in_line;
-    ifstream in("/home/plaif/global_path.txt");
+    ifstream in("/home/choiys/global_path.txt");
     double x_ = 0;
     double y_ = 0;
     while(getline(in,in_line)){
@@ -126,8 +126,8 @@ bool RAstarPlannerROS::makePlan(const geometry_msgs::PoseStamped& start, const g
       geometry_msgs::PoseStamped new_goal = goal;
       tf::Quaternion goal_quat = tf::createQuaternionFromYaw(atan2(x-x_,y-y_)+M_PI-M_PI/2 -M_PI/6);
 
-      new_goal.pose.position.x = x;
-      new_goal.pose.position.y = y;
+      new_goal.pose.position.x = x-6763.45482065;
+      new_goal.pose.position.y = y+40939.2641813;
 
       new_goal.pose.orientation.x = 0;
       new_goal.pose.orientation.y = 0;
